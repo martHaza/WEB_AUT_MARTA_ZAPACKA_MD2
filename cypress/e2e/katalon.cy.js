e24a384fad826ac9cc38fab7aba664cd4a89cb0c
martHaza <martazapacka17@gmail.com>
2025-05-19 19:47:37 +0300

    katalon-demo-cura pabeigts

---
+++ cypress/e2e/katalon.cy.js
@@ -0,0 +1,48 @@
+const { appPage } = require("../pageObjects/appPage");
+const { appSummaryPage } = require("../pageObjects/appSummaryPage");
+const { appHistoryPage } = require("../pageObjects/appHistoryPage");
+const { loginPage } = require("../pageObjects/loginPage")
+
+describe('katalon-demo-cura scenarios', () => {
+  it('Scenario 1 - Make an Appointment', () => {
+    loginPage.visit();
+    loginPage.appointmentButton.click();
+    loginPage.usernameField.type("John Doe");
+    loginPage.passwordField.type("ThisIsNotAPassword");
+    loginPage.loginButton.click();
+
+    // set the following values:
+    const facility = 'Seoul CURA Healthcare Center';
+    appPage.comboFacility.select(facility);
+    appPage.readmissionBox.check();
+    appPage.medicaidRadioButton();
+    appPage.calendar.click();
+    const day = '30';
+    const comment = 'CURA Healthcare Service';
+    appPage.commentField.type(comment);
+    appPage.bookAppointmentButton.click();
+
+    // validate that previously set values are correct
+    appSummaryPage.checkFacility.should("contain", facility);
+    appSummaryPage.checkReadmission.should("contain", "Yes");
+    appSummaryPage.checkProgram.should("contain", "Medicaid");
+    appSummaryPage.checkVisitDate.should("contain", day);
+    appSummaryPage.checkComment.should("contain", comment);
+  });
+
+  it('Scenario 2 - - Appointment history empty'), () => {
+    loginPage.visit();
+    loginPage.appointmentButton.click();
+    loginPage.usernameField.type("John Doe");
+    loginPage.passwordField.type("ThisIsNotAPassword");
+    loginPage.loginButton.click();
+    appPage.menuButton.click();
+    appHistoryPage.validateSidebar();
+    appHistoryPage.historyButton();
+    appHistoryPage.validateNoAppointment();
+
+  }
+});
+  
+
+
